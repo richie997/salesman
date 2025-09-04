f12b2b8e457c431491d82db0ea2f043651691fc9
Script Boy <115647152+richie997@users.noreply.github.com>
2025-09-05 01:02:46 +0200

    feat: mono repo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
+import { Input } from "@workspace/ui/components/input"
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World + apps/web</h1>
+        <h1 className="text-2xl font-bold">Hello World + apps/widget</h1>
         <Button size="sm">Button</Button>
-        <p>add(1, 2) = {add(1, 2)}</p>
+        <p>add(2, 2) = {add(2, 2)}</p>
+        <Input />
       </div>
     </div>
   )
